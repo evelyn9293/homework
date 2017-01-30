c947e9772468080070c5d38d6e31c8671fc2f767
evelyn9293 <xinyue.zhao9293@gmail.com>
2017-01-30 17:09:46 -0500

    Add files via upload

---
+++ 1.10.2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<memory>
+
+int main()
+{
+	int stack[10]; // create an array on stack
+	int* ip1 = stack[0];
+
+	int* ip2 = new int[10]; // create an array on the heap(using allocation)
+	delete[] ip2;
+} 
