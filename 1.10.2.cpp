623d2e069a3efb0000d2de90c6f69ac27dab834c
evelyn9293 <xinyue.zhao9293@gmail.com>
2017-01-30 17:21:46 -0500

    Update 1.10.2.cpp

--- 1.10.2.cpp
+++ 1.10.2.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	int stack[10]; // create an array on stack
-	int* ip1 = stack[0];
+	int* ip1 = &stack[0];
 
 	int* ip2 = new int[10]; // create an array on the heap(using allocation)
 	delete[] ip2;
